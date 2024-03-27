103f2a4c059d162ed3f36e6abbef6d2ad6623c75
Manish Acharya <me.acharyamanish@gmail.com>
2024-03-27 13:26:12 +0545

    27 work done
    all done

---
+++ file17.js
@@ -0,0 +1,9 @@
+let winningNumber = 19;
+let userGuess = +prompt("guess a number ")
+// console.log(typeof userGuess, userGuess);
+if(userGuess =winningNumber){
+    console.log("guess is right ");
+}else{
+    if9
+
+}
